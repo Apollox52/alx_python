540da413861a126e8b50361d86e58132c8b97e7d
mouad <mr.robott52@gmail.com>
2023-12-07 22:49:19 +0100

    Hey1

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,15 @@
 from sys import argv
 
-num_args = len(argv) - 1
-args_text = "argument" if num_args == 1 else "arguments"
+def main():
+    num_args = len(argv) - 1
+    args_text = "argument" if num_args == 1 else "arguments"
 
-print(f"{num_args} {args_text}:")
+    print(f"{num_args} {args_text}:")
 
-for i, arg in enumerate(argv[1:], start=1):
-    print(f"{i}: {arg}")
+    for i, arg in enumerate(argv[1:], start=1):
+        print(f"{i}: {arg}")
 
-print("." if num_args == 0 else "")
+    print("." if num_args == 0 else "")
+
+if __name__ == "__main__":
+    main()
