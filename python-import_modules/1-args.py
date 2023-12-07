22fdc8f7ae1b2869d4013a6e2ec4f865ca6457fb
mouad <mr.robott52@gmail.com>
2023-12-07 23:51:35 +0100

    Hey11

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -9,7 +9,7 @@ def print_arguments():
         print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
 
         for i, arg in enumerate(argv[1:], start=1):
-            print(f"{i}: {arg}\n" if i < num_args else f"{i}: {arg}")
+            print(f"{i}: {arg}")
 
 if __name__ == "__main__":
     print_arguments()
