506f3e7281f695347593702b1a679bb6a13be8dd
mouad <mr.robott52@gmail.com>
2023-12-08 18:34:30 +0100

    Hey11

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -8,13 +8,23 @@ def print_matrix_integer(matrix=[[]]):
     print()
 
 if __name__ == "__main__":
-    matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]
-    ]
+    matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    matrix2 = [[1, 2], [4, 5]]
+    matrix3 = [[1, 2], [4, 5], [7, 8]]
+    matrix4 = [[1]]
+    matrix5 = [[1], [2], [3], [4]]
 
-    print_matrix_integer(matrix)
-    print("--")
-    print_matrix_integer()
-    
+    print("case: matrix =", matrix1)
+    print_matrix_integer(matrix1)
+
+    print("case: matrix =", matrix2)
+    print_matrix_integer(matrix2)
+
+    print("case: matrix =", matrix3)
+    print_matrix_integer(matrix3)
+
+    print("case: matrix =", matrix4)
+    print_matrix_integer(matrix4)
+
+    print("case: matrix =", matrix5)
+    print_matrix_integer(matrix5)
