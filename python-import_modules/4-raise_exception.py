65b709c4841be547903ba8f12f3781ebbfe35bd2
mouad <mr.robott52@gmail.com>
2023-12-08 17:56:03 +0100

    Hey11

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -1,7 +1,2 @@
 def raise_exception():
-    try:
-        raise TypeError("Exception raised")
-    except TypeError as t:
-        print(t)
-
-raise_exception()
+    raise TypeError("Exception raised")
