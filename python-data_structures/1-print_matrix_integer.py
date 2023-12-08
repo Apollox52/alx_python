bace970e730233fe2e04503991ecc09ae2657306
mouad <mr.robott52@gmail.com>
2023-12-08 19:50:24 +0100

    Hey11

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,27 +1,9 @@
-def print_matrix_integer(matrix=[[]]):
+def print_matrix_integer(matrix=[]):
     for row in matrix:
         for i, num in enumerate(row):
-            # Use str.format() to print integers without casting to strings
-            if i < len(row) - 1:
-                print("{:d}".format(num), end=" ")
-            else:
+            if i == len(row) - 1:
                 print("{:d}".format(num))
-    # Print an empty line after each row of the matrix
-    print()
-
-if __name__ == "__main__":
-    matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    matrix2 = [[1, 2], [4, 5]]
-    matrix3 = [[1, 2], [4, 5], [7, 8]]
-    matrix4 = [[1]]
-    matrix5 = [[1], [2], [3], [4]]
-
-    print_matrix_integer(matrix1)
-    print("--")
-    print_matrix_integer(matrix2)
-    print("--")
-    print_matrix_integer(matrix3)
-    print("--")
-    print_matrix_integer(matrix4)
-    print("--")
-    print_matrix_integer(matrix5)
+            else:
+                print("{:d}".format(num), end=" ")
+    if not matrix:
+        print()
