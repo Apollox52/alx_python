9f3188f29ad090e76d80b63774e0eebfcf1ced9b
mouad <mr.robott52@gmail.com>
2023-12-08 17:42:02 +0100

    Hey11

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,18 +1,15 @@
-from sys import argv
-
-def print_arguments():
-    num_args = len(argv) - 1
-
-    if num_args == 0:
-        print("0 arguments.\n.")
-    else:
-        print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
-
-        for i, arg in enumerate(argv[1:], start=1):
-            print(f"{i}: {arg}")
+import sys
 
 if __name__ == "__main__":
-    print_arguments()
+    argv = sys.argv[1:]
+    argc = len(argv)
+
+    if argc == 0:
+        print("0 arguments.")
+    else:
+        print(f"{argc} {'argument' if argc == 1 else 'arguments'}:")
+        for i, arg in enumerate(argv, 1):
+            print(f"{i}: {arg}")
 
 
 
