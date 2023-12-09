e1475455de40d25b66da696461c0cb9db778106a
mouad <mr.robott52@gmail.com>
2023-12-09 16:36:46 +0100

    Hey11

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,12 +1,21 @@
-def print_matrix_integer(matrix=[]):
+def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        return
+
     for row in matrix:
         for i, num in enumerate(row):
             if i == len(row) - 1:
-                print("{:d}".format(num))
+                print(num, end="$\n")
             else:
-                print("{:d}".format(num), end=" ")
-    if not matrix:
-        print()
+                print(num, end=" ")
 
-matrix_empty = [[]]
-print_matrix_integer(matrix_empty)
+    print("--$")
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print_matrix_integer(matrix)
+print_matrix_integer()
