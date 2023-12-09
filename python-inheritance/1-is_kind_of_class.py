7ce42c982ed66bef1718428d7ef23f1d83846fe8
mouad <mr.robott52@gmail.com>
2023-12-09 20:27:09 +0100

    Hey11

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,12 @@
+def is_kind_of_class(obj, a_class):
+    """Returns True if the object is an instance of, or if the object is an instance of a class
+    that inherited from, the specified class; otherwise False.
+
+    Args:
+        obj: The object to check.
+        a_class: The class to check against.
+
+    Returns:
+        bool: True if obj is an instance of a_class or its subclasses, False otherwise.
+    """
+    return isinstance(obj, a_class)
