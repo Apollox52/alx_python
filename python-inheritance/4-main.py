0f39856864b84ad676fdee5beabca0c9b753db3f
mouad <mr.robott52@gmail.com>
2023-12-10 17:03:33 +0100

    Hey11

--- python-inheritance/4-main.py
+++ python-inheritance/4-main.py
@@ -6,9 +6,6 @@ BaseGeometry = __import__('base_geometry').BaseGeometry
 
 if __name__ == "__main__":
     # Example usage
-    bg = BaseGeometry() 
-    print(dir(bg))
-    
     bg = BaseGeometry()
 
     try:
@@ -16,5 +13,5 @@ if __name__ == "__main__":
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
 
-     
-    
+    # Additional case
+    print(dir(bg))
