41a183b2a5a244ea74f78cf7d6fd2f7a6ed12b37
mouad <mr.robott52@gmail.com>
2023-12-19 21:47:07 +0100

    Hey11

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,20 +1,26 @@
+
 class Base:
     """
     Base class for managing id attribute in all classes.
 
     Attributes:
-    - id (int): Public instance attribute representing the object identifier.
+        - id (int): Public instance attribute representing the object identifier.
     """
 
-    __nb_objects = 0  # private class attribute
+    __nb_objects = 0
+    """
+    Private class attribute to keep track of the number of objects created.
+
+    This attribute is used to generate unique identifiers for objects.
+    """
 
     def __init__(self, id=None):
         """
         Constructor for the Base class.
 
         Args:
-        - id (int, optional): Object identifier. If provided, assign to the id attribute.
-            If not provided, increment __nb_objects and assign the new value to id.
+            id (int, optional): Object identifier. If provided, assign to the id attribute.
+                If not provided, increment __nb_objects and assign the new value to id.
         """
         if id is not None:
             self.id = id
