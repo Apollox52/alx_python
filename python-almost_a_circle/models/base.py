252e284dc21dbe948dd80218c96f13bd52697367
mouad <mr.robott52@gmail.com>
2023-12-20 00:58:59 +0100

    Hey11

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,3 +1,11 @@
+"""
+models: A package for managing objects in the project.
+
+This package contains modules for defining base classes and other related functionality.
+
+Modules:
+    - base: Contains the Base class for managing object identifiers.
+"""
 class Base:
     """
     Base class for managing id attribute in all classes.
