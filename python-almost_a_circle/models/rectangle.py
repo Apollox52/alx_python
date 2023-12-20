aaafde55e63b379f2796f6f3449cdae20d85d0db
mouad <mr.robott52@gmail.com>
2023-12-20 01:49:10 +0100

    Hey11

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -141,15 +141,8 @@ class Rectangle(Base):
         return self.__width * self.__height
 
     def display(self):
-        """Print the rectangle using the '#' character."""
+        """Print the rectangle using the '#' character, considering x and y offsets."""
+        for _ in range(self.__y):
+            print()
         for _ in range(self.__height):
-            print("#" * self.__width)
-
-    def __str__(self):
-        """
-        Return a string representation of the rectangle.
-
-        Returns:
-            str: String representation of the rectangle.
-        """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+            print(" " * self.__x + "#" * self.__width)
