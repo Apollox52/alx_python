4b88959049359e72493d8183fca518a957637988
mouad <mr.robott52@gmail.com>
2024-01-24 17:28:44 +0100

    Hey11

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,16 @@
 import MySQLdb
 import sys
 
-def filter_states(username, password, database):
+def filter_states(username, password, database, state_name):
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
     # Create a cursor object to execute queries
     cursor = db.cursor()
 
-    # Execute the query to select states starting with 'N'
-    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id"
-    cursor.execute(query)
+    # Use format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = %s ORDER BY id"
+    cursor.execute(query, (state_name,))
 
     # Fetch all rows
     rows = cursor.fetchall()
@@ -25,12 +25,12 @@ def filter_states(username, password, database):
 
 if __name__ == "__main__":
     # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
+    if len(sys.argv) != 5:
+        print("Usage: python script.py <username> <password> <database> <state_name>")
         sys.exit(1)
 
     # Get command-line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
 
     # Call the function to filter and display states
-    filter_states(username, password, database)
+    filter_states(username, password, database, state_name)
