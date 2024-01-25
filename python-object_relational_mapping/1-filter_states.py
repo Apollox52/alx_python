f08c67755a46df0987d327d337ff0ee36bcdb59c
mouad <mr.robott52@gmail.com>
2024-01-25 03:03:00 +0100

    Hey11

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,33 @@
+#!/usr/bin/python3
 import MySQLdb
 import sys
 
-def filter_states(username, password, database):
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get command line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
     # Connect to MySQL server
     db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
 
-    # Create a cursor object to execute queries
+    # Create a cursor object using cursor() method
     cursor = db.cursor()
 
-    # Execute the query to select states starting with 'N' or 'n' (case-insensitive)
-    query = "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ORDER BY id"
+    # Execute SQL query to select states starting with 'N'
+    query = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;"
     cursor.execute(query)
 
-    # Fetch all rows
-    rows = cursor.fetchall()
+    # Fetch all the rows in a list of tuples
+    results = cursor.fetchall()
 
     # Display the results
-    for row in rows:
+    for row in results:
         print(row)
 
-    # Close the cursor and connection
+    # Close the cursor and database connection
     cursor.close()
     db.close()
-
-if __name__ == "__main__":
-    # Check if the correct number of command-line arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: python script.py <username> <password> <database>")
-        sys.exit(1)
-
-    # Get command-line arguments
-    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
-
-    # Call the function to filter and display states
-    filter_states(username, password, database)
