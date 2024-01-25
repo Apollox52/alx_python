4456c9bea69f70d69da821d14ea57278317eb233
mouad <mr.robott52@gmail.com>
2024-01-25 03:11:49 +0100

    Hey11

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -2,6 +2,36 @@
 import MySQLdb
 import sys
 
+def filter_states(username, password, database):
+    try:
+        # Connect to MySQL server
+        db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+
+        # Create a cursor object using cursor() method
+        cursor = db.cursor()
+
+        # Execute SQL query to select states starting with 'N' (case-insensitive)
+        query = "SELECT * FROM states WHERE name LIKE %s COLLATE utf8_general_ci ORDER BY id;"
+        cursor.execute(query, ('N%',))
+
+        # Fetch all the rows in a list of tuples
+        results = cursor.fetchall()
+
+        # Display the results
+        if results:
+            for row in results:
+                print(row)
+        else:
+            print("No records found.")
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+
+    finally:
+        # Close the cursor and database connection
+        cursor.close()
+        db.close()
+
 if __name__ == "__main__":
     # Check if the correct number of arguments is provided
     if len(sys.argv) != 4:
@@ -11,23 +41,5 @@ if __name__ == "__main__":
     # Get command line arguments
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
-
-    # Create a cursor object using cursor() method
-    cursor = db.cursor()
-
-    # Execute SQL query to select states starting with 'N' (case-insensitive)
-    query = "SELECT * FROM states WHERE name LIKE 'N%' COLLATE utf8_general_ci ORDER BY id;"
-
-
-    # Fetch all the rows in a list of tuples
-    results = cursor.fetchall()
-
-    # Display the results
-    for row in results:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    # Call the filter_states function
+    filter_states(username, password, database)
