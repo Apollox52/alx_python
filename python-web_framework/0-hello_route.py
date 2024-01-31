a0e7c43137dcd6007f08015d73e04072cfa2dcbc
mouad <mr.robott52@gmail.com>
2024-01-31 22:10:50 +0100

    Hey11

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,3 +1,12 @@
+"""
+0-hello_route.py - A simple Flask web application.
+
+This script starts a Flask web application listening on 0.0.0.0, port 5000.
+It defines a route '/' with the option strict_slashes=False, displaying 'Hello HBNB!'.
+
+Usage: python3 0-hello_route.py
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,11 +15,14 @@ app = Flask(__name__)
 def hello_hbnb():
     """
     Route to display 'Hello HBNB!'
+
+    Returns:
+        str: The greeting message.
     """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
     """
-    Run the Flask application on 0.0.0.0, port 5000
+    Run the Flask application on 0.0.0.0, port 5000.
     """
     app.run(host='0.0.0.0', port=5000)
