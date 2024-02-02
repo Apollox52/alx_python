842cc4af908fbb5c279868b5a5ef59687165d510
mouad <mr.robott52@gmail.com>
2024-02-02 02:44:46 +0100

    Hey11

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -85,10 +85,7 @@ def number_template(n):
     Returns:
         str: The HTML page or a 404 error if n is not an integer.
     """
-    if isinstance(n, int):
-        return render_template('5-number_template.html', n=n)
-    else:
-        abort(404)
+    return render_template('5-number_template.html', n=n)
 
 if __name__ == '__main__':
     """
