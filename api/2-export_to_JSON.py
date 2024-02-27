7b8b4a3bbd4ec7d7e8962d2337bbdf360316fc93
mouad <mr.robott52@gmail.com>
2024-02-27 21:22:05 +0100

    Hey11

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -3,6 +3,15 @@ import json
 import requests
 
 def get_user_tasks(user_id):
+    """
+    Fetch user information and tasks based on the provided user ID.
+
+    Args:
+        user_id (int): The ID of the user.
+
+    Returns:
+        tuple: A tuple containing user data (dict) and tasks data (list of dicts).
+    """
     # Fetching user data
     user_response = requests.get(f'https://jsonplaceholder.typicode.com/users/{user_id}')
     user_data = user_response.json()
@@ -14,6 +23,16 @@ def get_user_tasks(user_id):
     return user_data, tasks_data
 
 def export_to_json(user_id, tasks):
+    """
+    Export user tasks to a JSON file.
+
+    Args:
+        user_id (int): The ID of the user.
+        tasks (list): List of tasks for the user.
+
+    Returns:
+        None
+    """
     data_to_export = {str(user_id): [{"task": task["title"], "completed": task["completed"], "username": user_data["username"]} for task in tasks]}
     filename = f'{user_id}.json'
 
